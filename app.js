747a055f05776159ec471261e11c05e0fd17a04e
Keanu2431 <lumpkinkeanu131@gmail.com>
2022-09-17 04:59:50 -0400

    .

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.json());
 const findSpecific = function (arr, pid) {
   return arr.find((el) => Number(el.PID) === Number(pid));
 };
+const checkUserAndPass = function (user, pass) {
+  return allUsers.find((el) => {
+    return (
+      el.userName.toLowerCase() === user.toLowerCase() &&
+      el.userPassword === pass
+    );
+  });
+};
 // const item = allBras.find((el) => {
 //   return el.PID == 11205999;
 // });
@@ -30,6 +38,10 @@ const pushupData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/bras/push-up/push-up.json`)
 );
 const allBras = [...braletteData, ...laceData, ...straplessData, ...pushupData];
+// USSER DATA
+const allUsers = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users/user-acc.json`)
+);
 // ROUTE HANDLERS
 const getProducts = (req, res) => {
   let category = req.params.cat;
@@ -70,8 +82,43 @@ const getProducts = (req, res) => {
     });
   }
 };
+const verifyUser = (req, res) => {
+  const username = req.params.user;
+  const password = req.params.password;
+  if (username && password && checkUserAndPass(username, password)) {
+    res.status(200).json({
+      status: 'success',
+      data: checkUserAndPass(username, password),
+    });
+  } else {
+    res.status(404).json({
+      status: 'failed',
+      message: 'No user with this matching username and password',
+    });
+  }
+  // if (
+  //   allUsers.find((el) => {
+  //     return el.userName.toLowerCase() == username.toLowerCase();
+  //   })
+  // ) {
+  //   const verifiedUsername = allUsers.find((el) => {
+  //     return el.userName.toLowerCase() == username.toLowerCase();
+  //   });
+  //   console.log(`there is a user`, verifiedUsername);
+  //   if (verifiedUsername.userPassword === password) {
+  //     console.log('user is logged in');
+  //     res.status(200).json({
+  //       status: 'success',
+  //       data: verifiedUsername,
+  //     });
+  //   }
+  // }
+};
+
 // ROUTES
 app.route('/products/:cat?/:subCat?/:PID?').get(getProducts);
+app.route('/users/login/:user?/:password?').get(verifyUser);
+app.route('users');
 // updating data
 const editData = function (category) {
   braletteData.forEach((el) => {
@@ -82,13 +129,9 @@ const editData = function (category) {
     JSON.stringify(braletteData)
   );
 };
-// route handlers
-const getAllBras = (res, req) => {
-  res.status(200).json({
-    status: 'success',
-    results: braletteData.length,
-  });
-};
 app.listen(port, () => {
   console.log(`running on port:${port}`);
 });
+
+const keanu = checkUserAndPass('Keanu2431', 'B@bycat19');
+console.log(keanu);
